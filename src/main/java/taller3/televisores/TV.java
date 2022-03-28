9fee8950736a026ad04b715b227de4a3bc92c085
psga <pusugag@unal.edu.co>
2022-03-28 22:57:14 +0000

    p

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,86 @@
+package taller3.televisores;
+
+public class TV {
+
+    public static int numTV;
+
+    private Marca marca;
+    private int canal = 1;
+    private int precio = 500;
+    private boolean estado;
+    private int volumen = 1;
+    private Control control;
+
+    public TV(Marca marca, boolean estado) {
+        this.marca = marca;
+        this.estado = estado;
+        numTV++;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+    public void setControl(Control control) {
+        this.control = control;
+    }
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    public void setVolumen(int volumen) {
+        if (estado == true && volumen >=0 && volumen<=7)
+            this.volumen = volumen;
+    }
+    public void setCanal(int canal) {
+        if (estado == true && canal >0 && canal<=120)
+            this.canal = canal;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+    public Control getControl() {
+        return control;
+    }
+    public int getPrecio() {
+        return precio;
+    }
+    public int getVolumen() {
+        return volumen;
+    }
+    public int getCanal() {
+        return canal;
+    }
+
+    public static void setNumTV(int num) {
+        numTV = num;
+    }
+    public static int getNumTV() {
+        return numTV;
+    }
+    public void turnOn() {
+        estado = true;
+    }
+    public void turnOff() {
+        estado = false;
+    }
+
+    public boolean getEstado() {
+        return estado;
+    }
+    public void canalUp() {
+        if (estado == true && canal<120)
+            canal++;
+    }
+    public void canalDown() {
+        if (estado == true && canal>1)
+            canal--;
+    }
+    public void volumenUp() {
+        if (estado == true && volumen<7)
+            volumen++;
+    }
+    public void volumenDown() {
+        if (estado == true && volumen>1)
+            volumen--;
+    }
+}
